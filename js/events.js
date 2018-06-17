0283e225ea3b3587a42a00f5a36cebc7aa1b546d
Tyler Alder <tyler.b.alder@gmail.com>
2018-06-17 20:28:39 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,28 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $(document.body.form).on('keypress', function(key) {
+    if (key.which === 'g') {
+      alert('You have preseed the g key.');
+    }
+  })
+  
+}
+
+function submitIt() {
+  
+}
 
 $(document).ready(function(){
 
