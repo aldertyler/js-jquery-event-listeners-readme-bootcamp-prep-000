a5cef76230d478b18848e16a268451e0b215b804
Tyler Alder <tyler.b.alder@gmail.com>
2018-06-17 20:49:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,8 +12,8 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('input').on('keydown', function(e) {
-    if (e.key == 'g') {
+  $('input').on('keydown', function(key) {
+    if (key.which == 71) {
       alert('You have preseed the g key.');
     }
   })
